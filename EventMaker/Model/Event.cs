fffbb642a9c6ecf2f0e1e4f06ef5c0b4d5dddcc7
Rasmus Rohde Bergstrøm <rasm915w@edu.easj.dk>
2020-02-06 12:17:40 +0100

    EventMaker push

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class Event
+    {
+        // instance fields
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _place;
+        private DateTime _dateTime;
+
+        // constructor
+        public Event(int ID, string Name, string Description, string Place, DateTime DateTime)
+        {
+            _id = ID;
+            _name = Name;
+            _description = Description;
+            _place = Place;
+            _dateTime = DateTime;
+        }
+
+        // properties
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string Place
+        {
+            get { return _place; }
+            set { _place = value; }
+        }
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+        }
+
+        // tostring() method
+        public override string ToString()
+        {
+            return $"";
+        }
+    }
+}
---
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class EventCatalogSingleton
+    {
+        private static EventCatalogSingleton _instance;
+        private ObservableCollection<Event> _events;
+
+        public static EventCatalogSingleton Instance
+        {
+            get { return _instance; }
+        }
+        public EventCatalogSingleton()
+        {
+            _events = new ObservableCollection<Event>();
+        }
+        public ObservableCollection<Event> Events
+        {
+            get { return _events; }
+        }
+        public void Add()
+        {
+            _events.Add(Event);
+        }
+    }
+}
